501d7a227cc797cc2a08a0c67e96b6d8400555f8
Andreas Brem <andi.brem@web.de>
2026-03-20 12:51:06 +0100

    add: updateSettingsSelectionToStartText() refactor: small updates in _settings.scss

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
 import './scss/main.scss';
-    
-flipCard();
 
-function flipCard():void {
+// flipCard();
+
+function flipCard(): void {
     const fieldRef = document.getElementById('field');
     if (fieldRef) {
         fieldRef.addEventListener('click', e => {
             const card = (e.target as HTMLElement).closest('.card') as HTMLButtonElement;
-            if(card) {
+            if (card) {
                 card.classList.toggle('is-flipped')
             }
         })
     }
 }
 
-function settingsSelection() {
-    
+function updateSettingsSelectionToStartText(input: string): void {
+    const source = document.getElementById(input) as HTMLInputElement;
+    source?.addEventListener('onchange', () => {
+        let index: number = parseInt(input.substring(input.length - 1)) - 1;
+        let id: string = input.slice(0, input.length - 1)
+        const target = document.getElementById(id + 'Text') as HTMLElement;
+        target.innerText = source.value;
+    })
+}
+
+function settingsSelection(id: string) {
+    const elemRef = document.getElementById(id);
 }
